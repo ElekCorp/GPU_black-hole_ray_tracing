54fb467efa68130ae35cd72cb55a391e1add8ba8
Péter Elek <elekpeter2000@gmail.com>
2025-02-03 00:20:49 +0100

    adding an OpenMP version for CPU

---
+++ CPU/szinsaver.h
@@ -0,0 +1,73 @@
+#ifndef SZINSAVER_H
+#define SZINSAVER_H
+
+#include <iostream>
+#include <fstream>
+
+//#include "debugmalloc.h"
+
+
+int datasaver(int8_t* szin, int SZELES, int MAGAS, char* filename);
+int datasaver(int8_t* szin, int SZELES, int MAGAS, std::string& filename);
+
+template <class FP>
+inline int datasaver_T(FP* szin, int SZELES, int MAGAS, char* filename)
+{
+    std::ofstream wf(filename, std::ios::out | std::ios::binary);
+    if (!wf) {
+        std::cout << "Cannot open file!" << std::endl;
+        return 1;
+    }
+
+    int m = MAGAS;
+    int sz = SZELES;
+
+    wf.write((char*)&m, sizeof(int));
+    wf.write((char*)&sz, sizeof(int));
+
+    for (int i = 0; i < MAGAS * SZELES; i++)
+    {
+        float tmp = float(szin[i]);
+        wf.write((char*)&(tmp), sizeof(float));
+    }
+    wf.close();
+    if (!wf.good())
+    {
+        std::cout << "Error occurred at writing time!" << std::endl;
+        return 1;
+    }
+    return 0;
+
+}
+
+template <class FP>
+inline int datasaver_T(FP* szin, int SZELES, int MAGAS, std::string& filename)
+{
+    std::ofstream wf(filename, std::ios::out | std::ios::binary);
+    if (!wf) {
+        std::cout << "Cannot open file!" << std::endl;
+        return 1;
+    }
+
+    int m = MAGAS;
+    int sz = SZELES;
+
+    wf.write((char*)&m, sizeof(int));
+    wf.write((char*)&sz, sizeof(int));
+
+    for (int i = 0; i < MAGAS * SZELES; i++)
+    {
+        float tmp = float(szin[i]);
+        wf.write((char*)&(tmp), sizeof(float));
+    }
+    wf.close();
+    if (!wf.good())
+    {
+        std::cout << "Error occurred at writing time!" << std::endl;
+        return 1;
+    }
+    return 0;
+
+}
+
+#endif // SZINSAVER_H
